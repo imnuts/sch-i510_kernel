0e492851bec7d7f7237ba25799950e9fdf653dcc
Mark Dietz <mdietz@alum.bucknell.edu>
2011-04-27 22:27:03 -0400

    Undervolt to save battery

--- arch/arm/mach-s5pv210/max8998_consumer.c
+++ arch/arm/mach-s5pv210/max8998_consumer.c
@@ -86,11 +86,11 @@ enum PMIC_VOLTAGE {
 static const unsigned int frequency_match_1GHZ[][4] = {
 /* frequency, Mathced VDD ARM voltage , Matched VDD INT*/
 #if 1
-        {1000000, 1275, 1100, 0},
-        {800000, 1200, 1100, 1},
-        {400000, 1050, 1100, 2},
-        {200000, 950, 1100, 4},
-        {100000, 950, 1000, 5},
+        {1000000, 120, 1100, 0},
+        {800000, 1100, 1100, 1},
+        {400000, 950, 1100, 2},
+        {200000, 875, 1100, 4},
+        {100000, 875, 1000, 5},
 #else //just for dvs test
         {1000000, 1250, 1100, 0},
         {800000, 1250, 1100, 1},
@@ -114,10 +114,11 @@ const unsigned int (*frequency_match[2])[4] = {
 
 #if 0
 /*  voltage table */
-static const unsigned int voltage_table[16] = {
-	750, 800, 850, 900, 950, 1000, 1050,
-	1100, 1150, 1200, 1250, 1300, 1350,
-	1400, 1450, 1500
+static const unsigned int voltage_table[31] = {
+	750, 775, 800, 825, 850, 875, 900, 925,
+	950, 975, 1000, 1025, 1050, 1075, 1100, 1125,
+	1150, 1175, 1200, 1225, 1250, 1275, 1300, 1325,
+	1350, 1375, 1400, 1425, 1450, 1475, 1500
 };
 #endif
 
@@ -158,10 +159,10 @@ const unsigned int (*dvs_volt_table[2])[3] = {
 };
 
 static const unsigned int dvs_arm_voltage_set[][2] = {
-	{DVSARM1, 1275},
-	{DVSARM2, 1200},
-	{DVSARM3, 1050},
-	{DVSARM4, 950},
+	{DVSARM1, 1200},
+	{DVSARM2, 1100},
+	{DVSARM3, 950},
+	{DVSARM4, 875},
 	{DVSINT1, 1100},
 	{DVSINT2, 1000},
 };
