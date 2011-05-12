f7332d23975487f89ec87601ef5c129a85038e11
Mark Dietz <mdietz@alum.bucknell.edu>
2011-05-12 19:41:50 -0400

    Fix a command and move to Linaro Toolchain

--- build_kernel.sh
+++ build_kernel.sh
@@ -27,11 +27,11 @@ make clean mrproper > /dev/null 2>&1
 rm -f update_"$CONFIG"/*.zip update_"$CONFIG"/kernel_update/zImage
 
 make ARCH=arm stealth_defconfig 1>/dev/null 2>"$WORK"/errlog.txt
-make -j6 CROSS_COMPILE=/opt/toolchains/arm-2010q1/bin/arm-none-linux-gnueabi- ARCH=arm HOSTCFLAGS="-g -O3" 1>"$WORK"/stdlog.txt 2>>"$WORK"/errlog.txt
+make -j6 CROSS_COMPILE=/usr/bin/arm-linux-gnueabi- ARCH=arm HOSTCFLAGS="-g -O3" 1>"$WORK"/stdlog.txt 2>>"$WORK"/errlog.txt
 if [ $? != 0 ]; then
 		echo -e "FAIL!\n"
 		cd ..
-		mv .git fascinate_initramfs/
+		mv .git stealth_initramfs/
 		exit 1
 	else
 		echo -e "Success!\n"
@@ -41,7 +41,7 @@ fi
 cp arch/arm/boot/zImage update_"$CONFIG"/kernel_update/zImage
 cd update_"$CONFIG"
 zip -r -q kernel_update.zip .
-mv kernel_update.zip ../"$DATE"_imnuts_"$CONFIG".zip
+mv kernel_update.zip ../"$DATE"_stealth_"$CONFIG".zip
 cd ../../
 mv .git stealth_initramfs/
 cd $WORK
