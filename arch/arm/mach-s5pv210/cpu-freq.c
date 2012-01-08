26f5b189bfbb0353421e5558e59a396f58bd0dca
Mark Dietz <mdietz@alum.bucknell.edu>
2012-01-07 22:05:31 -0500

    Undervolt

--- arch/arm/mach-s5pv210/cpu-freq.c
+++ arch/arm/mach-s5pv210/cpu-freq.c
@@ -77,23 +77,23 @@ const unsigned long int_volt_max = 1250000;
 
 static struct s5pv210_dvs_conf dvs_conf[] = {
 	[L0] = {
-		.arm_volt   = 1275000,
-		.int_volt   = 1100000,
-	},
-	[L1] = {
 		.arm_volt   = 1200000,
 		.int_volt   = 1100000,
 	},
+	[L1] = {
+		.arm_volt   = 1100000,
+		.int_volt   = 1100000,
+	},
 	[L2] = {
-		.arm_volt   = 1050000,
+		.arm_volt   = 950000,
 		.int_volt   = 1100000,
 	},
 	[L3] = {
-		.arm_volt   = 950000,
+		.arm_volt   = 875000,
 		.int_volt   = 1100000,
 	},
 	[L4] = {
-		.arm_volt   = 950000,
+		.arm_volt   = 875000,
 		.int_volt   = 1000000,
 	},
 };
